5e464276a6999af54ebf57ecd3056fa45edb8daa
Pierre Hurtevent <pierre.hurtevent@knplabs.com>
2011-08-18 15:58:38 +0200

    Added some validations on Job model

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,9 @@
 class Job < ActiveRecord::Base
+  validates :title,         presence: true, length: { maximum: 255 }
+  validates :type,          presence: true, length: { maximum: 255 }
+  validates :location,      presence: true, length: { maximum: 255 }
+  validates :description,   presence: true, length: { maximum: 20000 }
+  validates :company_name,  presence: true, length: { maximum: 150 }
+  validates :company_email, presence: true, length: { maximum: 255 }
+  validates :company_url,   length: { maximum: 255 }
 end
